5897013a13393f433a83bf30db6845189b600bad
Andreas Hutagalung <andreashtgl7@gmail.com>
2022-10-05 15:07:46 +0700

    add controller and route 30%

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// frontsite
+use App\Http\Controllers\Frontsite\LandingController;
+use App\Http\Controllers\Frontsite\AppointmentController;
+use App\Http\Controllers\Frontsite\PaymentController;
+// use App\Http\Controllers\Frontsite\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', LandingController::class);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::group(['prefix' => 'backsite', 'as' => 'backsite.', 'middleware' => ['auth:sanctum', 'verified']], function () {
+
+    Route::resource('appointment', AppointmentController::class);
+
+    Route::resource('payment', PaymentController::class);
+
 });
